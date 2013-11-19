e5c28f30ebdd3e7b00c61d79fa6ad87bbef1a4e1
OpenShift guest <em222iv@student.lnu.se>
2013-11-19 10:19:08 -0500

    vandal

---
+++ 1-vandalen/guess/myjs.js
@@ -0,0 +1,28 @@
+"use strict";
+window.onload = function(){
+var makePerson = function(persArr){
+    
+    var splitedArray, ageArray, nameArray;
+    
+    splitedArray = persArr.split(',');
+    
+    ageArray = new Array(splitedArray[2], splitedArray[4], splitedArray[6]);
+    nameArray = new Array(splitedArray[1], splitedArray[3], splitedArray[5]);
+    console.log(ageArray);
+    
+    var person;
+	var names;
+	var minAge;
+	var maxAge;
+	var averageAge;
+	var personObj = {};
+	
+	
+	//personObj.averageAge = 
+	
+	//var totalAgeSum = persons.map(function(persons){
+    //return persons.age;
+	});
+    
+};
+};
--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,12 +1,46 @@
 "use strict";
 
 var makePerson = function(persArr){
+
+
+	// Din kod här...
+    var personAges,
+    personNames,
+    totalSum,
+    personObj = {};
     
     
-	var names;
-	var minAge;
-	var maxAge;
-	var averageAge;
+    personNames = persArr.map(function(personName){
+        return personName.name;
+    });
+    
+    personObj.names = personNames.sort(function(a, b){
+        return a.localeCompare(b)});
+    
+    //personObj.names = personNames;
+    
+    //Skapar en array som tar ut alla age från persArr arrayen
+    personAges = persArr.map(function(person){
+        return person.age;
+    });
+    
+    personObj.minAge = personAges.reduce(function(prevAge, age, i, personAges){
+        return Math.min(prevAge, age);
+    });
+    
+    personObj.maxAge = personAges.reduce(function(prevAge, age, i, personAges){
+        return Math.max(prevAge, age);
+    });
+    
+    totalSum = personAges.reduce(function(prevAge, age, i, personAges) {
+        return prevAge + age;
+    });
+    
+    personObj.averageAge = Math.round(totalSum/personAges.length);
+    //console.log(personAges); 
+    
+    return personObj;
     
 };
 
+  
