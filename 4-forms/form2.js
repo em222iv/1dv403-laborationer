41217fc4c155325c919618780e9c1a7d488c4ca1
OpenShift guest <em222iv@student.lnu.se>
2013-12-17 12:57:41 -0500

    fd

--- 4-forms/form2.js
+++ 4-forms/form2.js
@@ -1,84 +1,123 @@
 "use strict";
 
     var Validator = {
- 
-        validation : function() {
+        exist: null,
         
+        validation : function() {
         
         var nameCheck=document.getElementById("name");
         nameCheck.onblur = function() {
             
             if(nameCheck.value === null || nameCheck.value === "") {
-        
                 var errormessage=document.getElementById("errormessage");
-                var errorMessage=document.createTextNode("FEL! skriv ditt namn");
-                errormessage.appendChild(errorMessage);
-                return false;
-            }
-            
-           // Validator.blurred.errorMessage.parentNode.removeChild(Validator.blurred.errorMessage);
-            
-        };
+                
+                if(!this.exist){
+                    var p = document.createElement("p");
+                    
+                    var errorMessage=document.createTextNode("FEL! skriv ditt namn");
+                    p.id = "error";
+                    p.appendChild(errorMessage);
+                    errormessage.appendChild(p);
+                    this.exist = true;
+                }
+                }
+                else{
+    
+                    var getErrorMsg = document.getElementById("error");
+                    getErrorMsg.parentNode.removeChild(getErrorMsg);  
+                    this.exist = null;
+                }
+            };
         
+            
         var surNameCheck=document.getElementById("surname");
         surNameCheck.onblur = function(){
             
     
             if(surNameCheck.value === null || surNameCheck.value === "") {
-            
-                var errormessage1=document.getElementById("errormessage1");
-                var errorMessage1=document.createTextNode("FEL! skriv ditt efternamn");
-                errormessage1.appendChild(errorMessage1);
-                return false;
-            }
+            var errormessage1=document.getElementById("errormessage1");
+                
+                if(!this.exist){
+                    var p = document.createElement("p");
+                    
+                    var errorMessage1=document.createTextNode("FEL! skriv ditt efternamn");
+                    p.id = "error1";
+                    p.appendChild(errorMessage1);
+                    errormessage1.appendChild(p);
+                    this.exist = true;
+                }
+                }
+                else{
+    
+                    var getErrorMsg = document.getElementById("error1");
+                    getErrorMsg.parentNode.removeChild(getErrorMsg);  
+                    this.exist = null;
+                }
         };
 
         var postCodeCheck=document.getElementById("postalcode");
         postCodeCheck.onblur = function() {
             
-            var post = postCodeCheck.value.replace(/[^0-9]/g,"");
-            var reg = /^\d{5}$/;
+        var post = postCodeCheck.value.replace(/[^0-9]/g,"");
+        var reg = /^\d{5}$/;
             
-            if(reg.test(post) === true) {
-                postCodeCheck.value = post;
-            }
-            else if(reg.test(post) === false) {
+        if(reg.test(post) === true) {
+            postCodeCheck.value = post;
+            var getErrorMsg = document.getElementById("error2");
+            getErrorMsg.parentNode.removeChild(getErrorMsg);  
+            this.exist = null;
+        }
+        else if(reg.test(post) === false) {
     
-            console.log(reg.test(post));
+        console.log(reg.test(post));
                // post = postCodeCheck.replace("SE","");
                 //postCodeCheck = postCodeCheck.replace(" ", "");
                 
                 //(/^\d{3}\d{2}$/) || postCodeCheck.value.match(/^[SE]+\d{5}$/)|| postCodeCheck.value.match(/^[SE]+\d{5}$/)
         
-            var errormessage2=document.getElementById("errormessage2");
-            var errorMessage2=document.createTextNode("FEL! skriv din postkod");
-            errormessage2.appendChild(errorMessage2);
-            return false;
+        var errormessage2=document.getElementById("errormessage2");
+                
+        if(!this.exist){
+            var p = document.createElement("p");
+                    
+                var errorMessage2=document.createTextNode("FEL! skriv ditt postnummer");
+                p.id = "error2";
+                p.appendChild(errorMessage2);
+                errormessage2.appendChild(p);
+                this.exist = true;
+            }
         }
-    };
-    
-        
-            
+    /*else{
+
+            var getErrorMsg = document.getElementById("error2");
+            getErrorMsg.parentNode.removeChild(getErrorMsg);  
+            this.exist = null;
+            }*/
+        };
             var emailCheck=document.getElementById("email");
             emailCheck.onblur = function() {
             
                 if(!emailCheck.value.match(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/) || emailCheck.value === "") {
-
                     var errormessage3=document.getElementById("errormessage3");
-                    var errorMessage3=document.createTextNode("FEL! skriv din E-post adress");
-                    errormessage3.appendChild(errorMessage3);
-                   
-                    return false;
-                }   
-        };
-        
-        
-        var formbutton = document.getElementById("formname");
-        formbutton.onsubmit = function(){
-        
-        };
-    }
-};
+                
+                if(!this.exist){
+                    var p = document.createElement("p");
+                    
+                    var errorMessage3=document.createTextNode("FEL! skriv ditt postnummer");
+                    p.id = "error3";
+                    p.appendChild(errorMessage3);
+                    errormessage3.appendChild(p);
+                    this.exist = true;
+                    }
+                }
+                else{
+                    var getErrorMsg = document.getElementById("error3");
+                    getErrorMsg.parentNode.removeChild(getErrorMsg);  
+                    this.exist = null;
+                } 
+            };
+        },
+    };
 
 window.onload = function() {
   Validator.validation();
