9d5f2c49b1197afc37a6765319ac4ee39c7c6e50
OpenShift guest <em222iv@student.lnu.se>
2014-01-05 18:26:19 -0500

    few

--- PJ/file.js
+++ PJ/file.js
@@ -1,23 +1,26 @@
 "use strict";
 
-var memory = {
-    
+
+    var memory = {
+   
+                       
     popupWindow: function() { 
         var body = document.getElementById("body");
         var popup = document.createElement("div");
-        //popup.setAttribute("id", "popup");
         var header = document.createElement("popupHeader");
         var footer = document.createElement("popupFooter");
         var cancelButton = document.createElement("button");
         var cancelButtonText = document.createTextNode("X");
         
-        cancelButton.className = "button";
-        popup.className = "popup";
-        
+        cancelButton.className = "cancelButton";
+        popup.className = "popupWindow";
+        header.className = "windowHeader";
+       
+
         cancelButton.setAttribute("click");
         
         cancelButton.appendChild(cancelButtonText);
-        popup.appendChild(cancelButton);
+        header.appendChild(cancelButton);
         popup.appendChild(header);
         popup.appendChild(footer);
         body.appendChild(popup);
@@ -25,10 +28,12 @@ var memory = {
         
         cancelButton.onclick = function() { 
             popup.parentNode.removeChild(popup);
-        };
-    },
-    
-    clickButton: function() {
+            };
+
+        }, 
+            
+            
+   clickButton: function() {
             var that = this;
             var Memory = document.getElementById("memory");
             Memory.addEventListener("click", function() {
@@ -36,7 +41,6 @@ var memory = {
             },false);
         }
     };
-    
-window.onload = function() {
-    memory.clickButton();
+        window.onload = function() {
+            memory.clickButton();
 };
