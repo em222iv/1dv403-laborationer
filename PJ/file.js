37bbe38e59223561cc5b67a8cc7ead36015e6c8c
OpenShift guest <em222iv@student.lnu.se>
2014-01-07 13:38:59 -0500

    bakgrundfix+reset

--- PJ/file.js
+++ PJ/file.js
@@ -1,6 +1,5 @@
 "use strict";
 
-    
     var AJAX = {
    
     count : 0,
@@ -11,20 +10,21 @@
         var header = document.createElement("popupHeader");
         var footer = document.createElement("popupFooter");
         var cancelButton = document.createElement("button");
-        var overflowScroll = document.createElement("div");
-        
-        //var cancelButtonText = document.createTextNode("X");
+        var resetBackground = document.createElement("resetButton")
+     
+        var counter = 0;
+        var resetBackgroundText = document.createTextNode("Reset background");
         
         cancelButton.className = "cancelButton";
+        resetBackground.className = "resetBackground";
         popup.className = "popupWindow";
      
         cancelButton.setAttribute("click");
+        resetBackground.setAttribute("click");
       
         var xhr = new XMLHttpRequest();
-        
         var thumboImgContainer = document.createElement("div");
-        var thumbImgDiv = document.createElement("div");
-         thumboImgContainer.className = "thumbImg";
+        thumboImgContainer.className = "thumbImg";
         xhr.onreadystatechange = function() {
             if(xhr.readyState === 4) {
                 if(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
@@ -34,48 +34,46 @@
              
                     for (var i = 0; i < jsonImageReciever.length; i++) {
                         
-
                         var imgWidth = Math.max(jsonImageReciever[i].thumbWidth);
                         var imgHeight = Math.max(jsonImageReciever[i].thumbHeight);
                         console.log(imgWidth, imgHeight);
-                        
-                        
                         var thumbImg = document.createElement("img");
                         
                         thumbImg.setAttribute("src", jsonImageReciever[i].thumbURL);
                         thumboImgContainer.appendChild(thumbImg);
-                        jsonImageReciever[i].thumbURL.className = "thumbImgContainer";
-                        
-                        thumbImgDiv.setAttribute("class", "thumbImgDiv");
+                        thumboImgContainer.setAttribute("class", "thumbImgContainer");
                         thumbImg.setAttribute("class", "thumbImg");
- 
- 
-                        thumbImg.onclick = function() { 
-                            
-                            
+                        thumbImg.setAttribute("id", "thumbImg" + counter++);
+
+                        thumbImg.onclick = function(x) { 
+                        
+                            var URLimg = x.target.id.replace("thumbImg", ""); 
+                            //var URLimg = document.getElementById("thumbImg", counter); 
+                            var setBackground = jsonImageReciever[URLimg].URL;
+                            var main = document.getElementById("main");
+                            main.style.backgroundImage = "url(" + setBackground + ")";
+                            console.log(URLimg);
                         };
                     }
-                }
-                else{
-                    console.log("läsfel:" + xhr.status);
                 }
             }  
         };
         xhr.open("get", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
         xhr.send(null);
         
-      
-        
-       // cancelButton.appendChild(cancelButtonText);
+        resetBackground.appendChild(resetBackgroundText);
         header.appendChild(cancelButton);
+        header.appendChild(resetBackground);
         popup.appendChild(header);
-        popup.appendChild(thumbImgDiv);
-        popup.appendChild(overflowScroll);
         popup.appendChild(thumboImgContainer);
         popup.appendChild(footer);
         body.appendChild(popup);
         body.insertBefore(body.firstChild);
         
+        resetBackground.onclick = function() { 
+            that.main = document.getElementById("main");
+            that.main.style.backgroundImage = "url('img/background.jpg')";
+        };
         cancelButton.onclick = function() { 
             popup.parentNode.removeChild(popup);
             that.count = 0;
