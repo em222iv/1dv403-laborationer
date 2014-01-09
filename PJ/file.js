efc5b38922944fbbacb36c57a92217830c23351a
OpenShift guest <em222iv@student.lnu.se>
2014-01-09 11:55:17 -0500

    load&styling

--- PJ/file.js
+++ PJ/file.js
@@ -11,48 +11,49 @@
         var footer = document.createElement("popupFooter");
         var cancelButton = document.createElement("button");
         var resetBackground = document.createElement("resetButton");
+        var smallSizeWindow = document.createElement("smallSizeWindow");
+        var regularSizeWindow = document.createElement("regularSizeWindow");
+        var enlargeWindow = document.createElement("enlargeWindow");
+        var loadImg = document.createElement("img");
         var resetBackgroundCount = 1;
-        
-     
         var counter = 0;
-        var resetBackgroundText = document.createTextNode("Reset background");
+        
         
         cancelButton.className = "cancelButton";
         resetBackground.className = "resetBackground";
+        smallSizeWindow.className = "smallSizeWindow";
+        regularSizeWindow.className = "regularSizeWindow";
+        enlargeWindow.className = "enlargeWindow";
         popup.className = "popupWindow";
      
-        cancelButton.setAttribute("click");
-        resetBackground.setAttribute("click");
-      
-        
-       
-           footer.style.backgroundImage = "url('img/ajax-loader.gif')";
-        
-  
-        var xhr = new XMLHttpRequest();
+        var time = setTimeout(function(){
+             
+            loadImg.src="img/ajax-loader.gif";
+            footer.appendChild(loadImg);
+            
+        },10);
+         
         var thumbImgContainer = document.createElement("div");
         thumbImgContainer.className = "thumbImg";
+        
+        var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if(xhr.readyState === 4) {
                 if(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
                     console.log(xhr.responseText);
                    
                     var jsonImageReciever = JSON.parse(xhr.responseText);
-                    var imgWidth;
-                    var imgHeight;
+                    var imgWidth = 0;
+                    var imgHeight = 0;
                     for (var i = 0; i < jsonImageReciever.length; i++) {
-                    
-                        /*var imgHeight = Math.max(jsonImageReciever[i].thumbHeight);
-                        console.log(imgWidth, imgHeight);*/
                         
-                        if(jsonImageReciever[i].thumbWidth > 1 || jsonImageReciever[i].thumbWidth < 1000){
-                         that.imgWidth = Math.max(jsonImageReciever[i].thumbWidth);
+                        if(jsonImageReciever[i].thumbWidth > imgWidth){
+                         imgWidth = Math.max(jsonImageReciever[i].thumbWidth);
                         }
-                        if(jsonImageReciever[i].thumbHeight > 1 || jsonImageReciever[i].thumbHeight < 1000){
-                         that.imgHeight = Math.max(jsonImageReciever[i].thumbHeight);
+                        if(jsonImageReciever[i].thumbHeight > imgHeight){
+                         imgHeight = Math.max(jsonImageReciever[i].thumbHeight);
                         }
-                        console.log(that.imgWidth, that.imgHeight);
-
+                        
                         var thumbImg = document.createElement("img");
                         thumbImg.setAttribute("src", jsonImageReciever[i].thumbURL);
                         thumbImgContainer.appendChild(thumbImg);
@@ -64,8 +65,8 @@
                         thumbImgDiv.setAttribute("id", "thumbImgDiv");
                         thumbImgContainer.appendChild(thumbImgDiv);
                         thumbImgDiv.appendChild(thumbImg);
-                        thumbImgDiv.style.width = that.imgWidth + 10 +"px";
-                        thumbImgDiv.style.height = that.imgHeight + 10 +"px";
+                        thumbImgDiv.style.width = imgWidth + 10 +"px";
+                        thumbImgDiv.style.height = imgHeight + 10 +"px";
 
                         thumbImg.onclick = function(x) { 
                         
@@ -74,27 +75,57 @@
                             var setBackground = jsonImageReciever[URLimg].URL;
                             var main = document.getElementById("main");
                             main.style.backgroundImage = "url(" + setBackground + ")";
-                            console.log(URLimg);
+                       
                             resetBackgroundCount = 0;
                         };
-                    }
-                }
+                    }clearTimeout(time); 
+                    footer.removeChild(loadImg);
+                }   
             }  
         };
         xhr.open("get", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
         xhr.send(null);
         
-        resetBackground.appendChild(resetBackgroundText);
         header.appendChild(cancelButton);
         header.appendChild(resetBackground);
+        header.appendChild(enlargeWindow);
+        header.appendChild(regularSizeWindow);
+        header.appendChild(smallSizeWindow);
         popup.appendChild(header);
         popup.appendChild(thumbImgContainer);
         popup.appendChild(footer);
         body.appendChild(popup);
         body.insertBefore(body.firstChild);
+         
+        smallSizeWindow.onclick = function() { 
+            popup.className = "popupWindow";
+            console.log(popup);
+            popup.style.height = "250px";
+            popup.style.width = "250px";
+            thumbImgContainer.style.height = "195px";
+            thumbImgContainer.style.width = "245px";
         
+        }; 
+        regularSizeWindow.onclick = function() { 
+            popup.className = "popupWindow";
+            console.log(popup);
+            popup.style.height = "400px";
+            popup.style.width = "365px";
+            thumbImgContainer.style.height = "345px";
+            thumbImgContainer.style.width = "360px";
+        
+        }; 
+        enlargeWindow.onclick = function() { 
+            popup.className = "popupWindow";
+            console.log(popup);
+            popup.style.height = "450px";
+            popup.style.width = "570px";
+            thumbImgContainer.style.height = "345px";
+            thumbImgContainer.style.width = "565px";
+        
+        }; 
+     
         resetBackground.onclick = function() { 
-            
             if(resetBackgroundCount > 0){
                 return;
             }
@@ -105,7 +136,10 @@
             }
             resetBackgroundCount++;
         };
+       
         cancelButton.onclick = function() { 
+            popup.className = "popupWindow";
+            console.log(popup);
             popup.parentNode.removeChild(popup);
             that.count = 0;
             };
